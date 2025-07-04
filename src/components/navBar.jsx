65398433f2ce997d467c7acf4f9ea7d6dcc26c41
AntonioBernalI <a01772490@tec.mx>
2025-07-04 13:59:13 -0600

    Updated navBar.jsx

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -61,7 +61,7 @@ const NavItem = styled.div`
     padding: 16px 20px;
     color: white;
     font-weight: bold;
-    font-size: 28px;
+    font-size: 25px;
     font-family: 'Overpass', sans-serif;
     text-transform: uppercase;
     letter-spacing: 1px;
