cd6134916bf0685d500a336cdf15bd3dba79909d
AntonioBernalI <a01772490@tec.mx>
2025-07-04 13:42:17 -0600

    Reduce navbar height on mobile

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,8 +2,17 @@ import {styled} from 'styled-components'
 const Nav = styled.div`
     margin-top: 1%;
     width: 100%;
-    height: 12%;
     background: radial-gradient(circle,rgba(255, 145, 0, 1) 3%, rgba(255, 110, 0, 1) 56%, rgba(255, 76, 0, 1) 90%);
+    
+    /* Desktop height */
+    @media (min-width: 800px) {
+        height: 12%;
+    }
+    
+    /* Mobile height - smaller for better proportions */
+    @media (max-width: 799px) {
+        height: 8%;
+    }
 `
 const NavBar = ({}) => {
     return (
