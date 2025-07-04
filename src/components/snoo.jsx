bd18d6e84fb6d6eb02bfa4b960a0299c4d6a8d86
AntonioBernalI <a01772490@tec.mx>
2025-07-04 13:37:34 -0600

    Position Snoo 15% from left and bottom

--- src/components/snoo.jsx
+++ src/components/snoo.jsx
@@ -3,21 +3,19 @@ import snoo from '../assets/snoo.png'
 const SnooDiv = styled.div`
     z-index: 90;
     position: absolute;
+    left: 15%;
+    bottom: 15%;
     
     /* PC styles (800px and above) */
     @media (min-width: 800px) {
         height: 200px;
         width: 200px;
-        bottom: -100px;
-        left: -100px;
     }
     
     /* Mobile styles (below 800px) */
     @media (max-width: 799px) {
         height: 150px;
         width: 150px;
-        bottom: -75px;
-        left: -75px;
     }
 `
 const Snoo = () => {
