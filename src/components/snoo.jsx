d37b61911971f983f8ac523b6b4f7fa50aa0cc6e
AntonioBernalI <a01772490@tec.mx>
2025-07-04 13:38:12 -0600

    Update Snoo position for PC

--- src/components/snoo.jsx
+++ src/components/snoo.jsx
@@ -3,19 +3,21 @@ import snoo from '../assets/snoo.png'
 const SnooDiv = styled.div`
     z-index: 90;
     position: absolute;
-    left: 15%;
-    bottom: 15%;
     
     /* PC styles (800px and above) */
     @media (min-width: 800px) {
         height: 200px;
         width: 200px;
+        left: 10%;
+        bottom: 10%;
     }
     
     /* Mobile styles (below 800px) */
     @media (max-width: 799px) {
         height: 150px;
         width: 150px;
+        left: 15%;
+        bottom: 15%;
     }
 `
 const Snoo = () => {
