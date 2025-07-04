05c1724928b474279ac6e44be4c13b3cd2cfe637
AntonioBernalI <a01772490@tec.mx>
2025-07-04 14:03:12 -0600

    Updated contentScreen.jsx

--- src/components/contentScreen.jsx
+++ src/components/contentScreen.jsx
@@ -2,7 +2,7 @@ import { styled } from 'styled-components'
 
 const ContentContainer = styled.div`
     width: 99%;
-    height: 60%;
+    height: 58%;
     margin-top: 2%;
     background: rgba(18, 103, 154, 0.9);
     border-radius: 15px;
