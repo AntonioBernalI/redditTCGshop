bcbb7d75cfe4b645d4157e4266fd0e248c3345e8
AntonioBernalI <a01772490@tec.mx>
2025-07-04 20:27:27 -0600

    Updated snoo.jsx

--- src/components/snoo.jsx
+++ src/components/snoo.jsx
@@ -9,7 +9,7 @@ const SnooDiv = styled.div`
         height: 300px;
         width: 300px;
         left: 8%;
-        bottom: 2%;
+        bottom: 1%;
     }
     
     /* Mobile styles (below 800px) */
