fd72eb210e23b73266622387e4338caea65e970f
Antonio Bernal Ibañez <a01772490@tec.mx>
2025-07-05 17:52:54 -0600

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useState } from "react";
+import { useEffect } from "react";
 import MainDiv from "./components/mainDiv.jsx";
 import Snoo from "./components/snoo.jsx";
 import HeaderImage from "./components/headerImage.jsx";
@@ -9,10 +9,10 @@ import MoneyDisplay from "./components/moneyDiv.jsx";
 import FullscreenBackdrop from "./components/FullscreenBackdrop.jsx";
 
 function deepFindMessage(obj) {
-  if (!obj || typeof obj !== 'object') return undefined;
-  if ('message' in obj) return obj.message;
+  if (!obj || typeof obj !== "object") return undefined;
+  if ("message" in obj) return obj.message;
 
-  if ('data' in obj) return deepFindMessage(obj.data);
+  if ("data" in obj) return deepFindMessage(obj.data);
 
   return undefined;
 }
@@ -23,23 +23,25 @@ function App() {
 
   useEffect(() => {
     // Send webview_ready message after mount
-    window.parent.postMessage({
-      type: 'webview_ready',
-      data: {}
-    }, '*');
-    
-    const handleMessage = (event) => {
+    window.parent.postMessage(
+      {
+        type: "webview_ready",
+        data: {},
+      },
+      "*"
+    );
+    const handleMessage = (event) => { 
       const message = deepFindMessage(event.data);
       if (!message) return;
       const { type, data } = message;
-      if (type==="balance_update"){
+      if (type === "balance_update") {
         setMoney(data);
       }
     };
 
-    window.addEventListener('message', handleMessage);
+    window.addEventListener("message", handleMessage);
     return () => {
-      window.removeEventListener('message', handleMessage);
+      window.removeEventListener("message", handleMessage);
     };
   }, []);
 
@@ -55,16 +57,17 @@ function App() {
       <FullscreenBackdrop />
       <MoneyDisplay money={money} />
       <MainDiv>
-          <HeaderImage />
-          <NavBar 
-            activeItem={activeNavItem} 
-            onItemClick={handleNavItemClick}
-          />
-          <ContentScreen activeItem={activeNavItem} money={money} updateMoney={updateMoney} />
+        <HeaderImage />
+        <NavBar activeItem={activeNavItem} onItemClick={handleNavItemClick} />
+        <ContentScreen
+          activeItem={activeNavItem}
+          money={money}
+          updateMoney={updateMoney}
+        />
       </MainDiv>
-        <Snoo/>
+      <Snoo />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
