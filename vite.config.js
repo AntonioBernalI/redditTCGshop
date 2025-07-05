aafba967703ae6683c5f4ce18663d16f3e9a67ae
Antonio Bernal Ibañez <a01772490@tec.mx>
2025-07-05 01:18:27 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Carpeta de salida para la build
+    outDir: 'shop',
+    /**
+     * (opcional) vacía la carpeta antes de cada build;
+     * si prefieres conservar archivos previos, pon false
+     */
+    emptyOutDir: true
+  }
 })
